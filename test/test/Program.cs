0083726b8e21410e3bdbb78015d02d05d5260c8d
Nabil <Nabil@DESKTOP-TRD4TSL>
2019-04-12 21:41:14 +0100

    No changes

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -9,20 +9,28 @@ namespace test
     {
         static void Main(string[] args)
         {
-            string[] data = File.ReadAllLines("Location1.CSV");
+            string[] dataLocation1 = File.ReadAllLines("Location1.CSV");
+            string[] dataLocation2 = File.ReadAllLines("Location2.CSV");
             List<Vehicles> Location1 = new List<Vehicles>();
             string[] bits;
-            string Location = data[0];
-            for(int i = 1; i < data.Length; i++)
+            string Location = dataLocation1[0];
+            for (int i = 1; i < dataLocation1.Length; i++)
             {
-                bits = data[i].Split(',');
-                if(bits[0] == "Car")
+                bits = dataLocation1[i].Split(',');
+                if (bits[0] == "Car")
                 {
                     Car c = new Car(bits[1], bits[2], int.Parse(bits[3]), int.Parse(bits[4]), int.Parse(bits[5]), bits[6], bits[7], int.Parse(bits[8]), double.Parse(bits[9]), bool.Parse(bits[10]));
                     Location1.Add(c);
                 }
             }
             Console.ReadKey();
+
+
+
+
+
+
         }
+
     }
 }
